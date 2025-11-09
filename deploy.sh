1276b9f7be8469f4f79e7ed96731717ca5a098f7
Sadika Rahman <116261624+srahman03@users.noreply.github.com>
2025-11-09 13:28:05 +0000

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo "Stopping processes"
+
+sudo systemctl stop nginx
+sudo systemctl stop project
+
+echo "Checking python is installed"
+command python3 --version > /dev/null 2>&1
+
+if [[ $? != 0 ]]; then
+        echo "installing python"
+        sudo apt install python3 -y
+fi
+
+echo "Checking python virtual environment is installed"
+if [[ -d ".venv" ]]; then
+        echo "Not needed to create virtual env"
+else
+         sudo apt install python3-venv -y
+         python3 -m venv .venv
+fi
+
+echo "Checking nginx is installed"
+command -v nginx > /dev/null 2>&1
+
+if [[ $? == 1 ]]; then
+        sudo apt install nginx -y
+else
+        sudo systemctl start nginx
+fi
+
+echo "activating virtual environment"
+sudo chmod 770 .venv/bin/activate
+source .venv/bin/activate
+
+echo "finished activating virtual environment"
+
+while IFS= read -r library;do
+        if [[ ! -d ".venv/lib/python3.12/site-packages/$library"  ]]; then
+                pip install "$library"
+        fi
+done < req_pip.txt
+
+echo "all installed"
+
+echo "Running app"
+sudo systemctl start project
+
+deactivate
+
+echo "started nginx"
+sudo systemctl start nginx
