793112e449e1f58dc43d4533b773fc75bc7d9dab
srahman03 <sadikar03@gmail.com>
2025-12-11 11:02:33 +0000

    fixed dir path

--- data-shipper/shipper-app.py
+++ data-shipper/shipper-app.py
@@ -3,7 +3,7 @@ import requests
 url = "https://elastic-latest.netbuilder-training.com/_bulk"
 headers = {"Content-Type": "application/x-ndjson"}
 
-with open("flask/file.txt", "rb") as f:
+with open("/data/file.txt", "rb") as f:
     data = f.read()
 
 response = requests.post(
