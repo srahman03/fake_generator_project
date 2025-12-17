b20f1794f4a60524c12da3b747a4f9af4d7d9e3e
srahman03 <sadikar03@gmail.com>
2025-12-17 17:07:43 +0000

    added elasticsearch python client

--- data-shipper/shipper-app.py
+++ data-shipper/shipper-app.py
@@ -1,21 +1,110 @@
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import streaming_bulk
 import requests
 import os
 from dotenv import load_dotenv
+import json
 
-load_dotenv()
-url = "https://elastic-latest.netbuilder-training.com/_bulk"
+schema_data = {
+    "wins": {"type":"int","min":0,"max":100},
+    "name": {"type":"name"},
+    "username":{"type":"username"},
+    "driver":{"type":"driver"},
+    "ip_address": {"type":"ip"},
+    "distance_of_track":{"type":"distance"},
+    "date_of_race":{"type":"date"},
+    "podiums_for_driver":{"type":"int","min":0,"max":250},
+    "latitude":{"type":"latitude"},
+    "longitude":{"type":"longitude"},
+    "circuit":{"type":"circuit"},
+    "date_of_birth":{"type":"dob"},
+    "text":{"type":"text"},
+    "team_for_driver":{"type":"team"},
+    "tyres":{"type":"tyres"},
+    "weather":{"type":"weather"},
+    "logs":"log"
+}
 
-headers = {"Content-Type": "application/x-ndjson"}
+def create_index(client):
+    pipeline_body = {
+        "description": "my index parser",
+        "processors": [
+            {
+                "grok": {
+                    "field": "logs",
+                    "patterns": [
+                        "%{DAY:log.day} %{MONTH:log.month} %{MONTHDAY:log.month-day} %{TIMESTAMP_ISO8601:log.timestamp} %{IP:log.ip} %{WORD:log.my_word} %{NUMBER:log.http_code} %{GREEDYDATA:log.log_line}"
+                    ]
+                }
+            },
+            {
+                "geoip": {
+                    "field": "log.ip",
+                    "target_field": "geoip"
+                }
+            }
+        ]
+    }
+    # Create the pipeline
+    client.ingest.put_pipeline(id="my_pipeline", body=pipeline_body)
 
-with open("/data/file.txt", "rb") as f:
-    data = f.read()
+    mapping = {
+        "settings": {"number_of_shards": 1},
+        "mappings": {
+            "properties": {
+                "location": {"type": "geo_point"}
+            }
+        },
+    }
+    client.indices.create(index="my_f1_index", body=mapping, ignore=400)
 
-response = requests.post(
-    url,
-    headers=headers,
-    data=data,
-    auth=("elastic", os.getenv("PASS"))
-)
+def generate_actions(flask_url,params,headers):
 
-print(response.text)
+    for _ in range(1, 10):
+        response = requests.post(
+            flask_url,
+            params=params,
+            json=schema_data,
+            headers=headers
+        )
 
+        if headers["Accept"] == "application/x-ndjson":
+            lines = []
+            for c in response.iter_lines():
+                lines.append(json.dumps(str(c)))
+                yield {
+                    "_index": "my_f1_index",
+                    "_source": c,
+                    "pipeline": "my_pipeline"
+                }
+
+        else:
+            for doc in response.json():
+                yield {
+                    "_index": "my_f1_index",
+                    "_source": doc,
+                    "pipeline": "my_pipeline"
+                }
+
+def main():
+    print("Downloading dataset...")
+    load_dotenv()
+    client = Elasticsearch(
+        'https://elastic-latest.netbuilder-training.com',
+        basic_auth=("elastic", os.getenv("PASS")))
+    print("Creating index...")
+    create_index(client)
+
+    #Sending to elastic api
+    flask_url = "https://flask-app-sr.netbuilder-training.com/api/schema"
+    params = {"count": 10}
+    headers = {"Accept": "application/json"}
+    print("Indexing documents...")
+    successes = 0
+    for ok, _ in streaming_bulk(client, index="my_f1_index", actions=generate_actions(flask_url,params,headers)):
+        successes += ok
+    print(f"Indexed {successes} documents")
+
+
+if __name__ == "__main__":
+    main()
--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request,jsonify, Response
 from myApp import generate_fake_data
 import json
-import hashlib
+
 app = Flask(__name__)
 
 @app.get("/api/schema")
@@ -20,19 +20,10 @@ def add_schema():
     count =  request.args.get('count',default=1)
     result = generate_fake_data(data, int(count))
     accept = request.headers.get("Accept")
-    index_name = "f1_index_2"
-    lines=[]
+
     lines_for_ndjson = []
     for i in result:
-        doc_id = hashlib.sha256(json.dumps(i, sort_keys=True).encode()).hexdigest()
-        lines.append(json.dumps({"index": {"_index": index_name, "_id": doc_id}}))
-        lines.append(json.dumps(i))
         lines_for_ndjson.append(json.dumps(i))
-    bulk_api_str = "\n".join(lines) + "\n"
-
-    with open('file.txt', "w") as f:
-        f.write(bulk_api_str)
-
     if "application/x-ndjson" in accept:
         ndjson_str = "\n".join(lines_for_ndjson) + "\n"
 
